e15806ed4a279599c865ab82f76a5fe9e078bd03
Nguyen Ngoc Hai Dang <haidangne203@gmail.com>
2026-03-15 02:47:35 +0700

    them goi va ghe dua vao group

--- src/config/uiGroups.js
+++ src/config/uiGroups.js
@@ -1,5 +1,6 @@
 import { SOFA_GROUP } from './sofaGroup.js';
 import { LAMP_GROUP } from './lampGroup.js';
 import { TV_STAND_GROUP } from './tvStandGroup.js';
-
-export const UI_GROUPS = [SOFA_GROUP, LAMP_GROUP, TV_STAND_GROUP];
+import { PILLOW_GROUP } from './pillowGroup.js';
+import { BACKREST_CHAIR_GROUP } from './backrestChairGroup.js';
+export const UI_GROUPS = [SOFA_GROUP, LAMP_GROUP, TV_STAND_GROUP, PILLOW_GROUP, BACKREST_CHAIR_GROUP];
